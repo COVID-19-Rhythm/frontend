4bc5a3fa5331d7956874a563cb35466e5077886e
crystal <noelani.villanueva@gmail.com>
2021-08-30 16:24:21 -0700

    landing

---
+++ rhythm-app/src/components/AboutSummary/AboutSummary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutSummary() {
+  return <div>about summary</div>;
+}
+
+export default AboutSummary;
---
+++ rhythm-app/src/components/Landing/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hoem from "../Home/index";
+import AboutSummary from "../AboutSummary/AboutSummary";
+import Research from "../Research/index";
+
+function Index() {
+  return (
+    <div>
+      <Hoem />
+      <AboutSummary />
+      <Research />
+    </div>
+  );
+}
+
+export default Index;
