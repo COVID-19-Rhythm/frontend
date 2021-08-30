2d14bb160eb9e19c311204500bed7ee6e59e17be
crystal <noelani.villanueva@gmail.com>
2021-08-30 16:13:54 -0700

    home

--- rhythm-app/src/App.js
+++ rhythm-app/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import About from "./components/About/index";
-import Contact from "./components/Contact/index";
-import Dashboard from "./components/Dashboard/index";
 import Landing from "./components/Research/index";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
