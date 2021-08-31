886ef1bc446efe9780ff2f085ab69c65dae2eaa4
crystal <noelani.villanueva@gmail.com>
2021-08-30 18:16:48 -0700

    reearch page

--- rhythm-app/src/components/Research/Research.js
+++ rhythm-app/src/components/Research/Research.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./research.css";
 
 function Research() {
-  return <div>research</div>;
+  return (
+    <div className="researchParent">
+      <div className="research">
+        <h1>Research</h1>
+      </div>
+      <div className="researhChildParent">
+        <div className="researchChild">
+          Mention Background here about research
+        </div>
+        <div className="researchChild2">*insert picture here *</div>
+      </div>
+    </div>
+  );
 }
 
 export default Research;
