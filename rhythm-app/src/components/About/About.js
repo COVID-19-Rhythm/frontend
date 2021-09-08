6075c95ecb74bb52096d724cabd601395e9a9de2
crystal <noelani.villanueva@gmail.com>
2021-09-07 18:45:15 -0700

    sources

--- rhythm-app/src/components/About/About.js
+++ rhythm-app/src/components/About/About.js
@@ -2,53 +2,51 @@ import React from "react";
 import "./about.css";
 import { Row, Column, Container, Col, Image } from "react-bootstrap";
 import Navbar from "../Navbar/Navbar";
-import testing from '../images/covid_testing.jpeg';
+import testing from "../images/microbiology.jpg";
 const aboutStyle = {
   container: {
-    minWidth:"100vw",
-
+    minWidth: "100vw",
   },
   row: {
-    backgroundColor:"rgba(203, 194, 194, 0.42)",
-    borderRadius:"25px",
-    padding:"4em",
-    minHeight:"80vh",
-    margin:"2em"
-  }
-}
+    backgroundColor: "rgba(203, 194, 194, 0.42)",
+    borderRadius: "25px",
+    padding: "4em",
+    minHeight: "80vh",
+    margin: "2em",
+  },
+};
 function About() {
   return (
     <div className="about-parent">
       <Container style={aboutStyle.container}>
-      <Row style={aboutStyle.row}>
+        <Row style={aboutStyle.row}>
           <h1 className="header">Inspiration</h1>
-          <br/>
+          <br />
           <Col>
-          <p>
-          We strive to provide easier and faster ways to test for COVID-19 that will keep people safer and healthier.
-          </p>
-          <p>
-            Rhythm is built to help people get more insight into their personal well being and help individuals keep their communities safe.
-          </p>
-          {/* <Image></Image> */}
+            <p>
+              We strive to provide easier and faster ways to test for COVID-19
+              that will keep people safer and healthier.
+            </p>
+            <p>
+              Rhythm is built to help people get more insight into their
+              personal well being and help individuals keep their communities
+              safe.
+            </p>
           </Col>
         </Row>
         <Row style={aboutStyle.row}>
-          
-          <br/>
-          <Col style={{width:"60%"}}>
-          {/* <p>
+          <br />
+          <Col style={{ width: "60%" }}>
+            {/* <p>
           Our technology is completely free for anyone to use.
           </p> */}
-          <h1 className="header">Free and Open Source for everyone!</h1>
+            <h1 className="header">Free and Open Source for everyone!</h1>
           </Col>
-          
-          <Col style={{width:"40%"}}>
-          <Image src={testing} alt="covid test" className="image-test"/>
+
+          <Col style={{ width: "40%" }}>
+            <Image src={testing} alt="covid test" className="image-test" />
           </Col>
         </Row>
-        
-        
       </Container>
     </div>
   );
--- rhythm-app/src/components/Sources/Sources.js
+++ rhythm-app/src/components/Sources/Sources.js
@@ -6,9 +6,37 @@ function Sources() {
     <div>
       <div className="sourcesParent">
         <h1>Sources</h1>
-        <p>source1</p>
-        <p>source1</p>
-        <p>source1</p>
+        <div className="sources">
+          <li>
+            Alavi, A., Bogu, G. K., Wang, M., Rangan, E. S., Brooks, A. W.,
+            Wang, Q., Higgs, E., Celli, A., Mishra, T., Metwally, A. A., Cha,
+            K., Knowles, P., Alavi, A. A., Bhasin, R., Panchamukhi, S., Celis,
+            D., Aditya, T., Honkala, A., Rolnik, B., … Snyder, M. P. (2021, June
+            21). Real-time alerting system for covid-19 using wearable data.
+            medRxiv : the preprint server for health sciences.
+            https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8240687/.{" "}
+          </li>
+          <li>
+            Alavi, A., Bogu, G. K., Wang, M., Rangan, E. S., Brooks, A. W.,
+            Wang, Q., Higgs, E., Celli, A., Mishra, T., Metwally, A. A., Cha,
+            K., Knowles, P., Alavi, A. A., Bhasin, R., Panchamukhi, S., Celis,
+            D., Aditya, T., Honkala, A., Rolnik, B., … Snyder, M. P. (2021,
+            January 1). Real-time alerting system for COVID-19 using wearable
+            data. medRxiv.
+            https://www.medrxiv.org/content/10.1101/2021.06.13.21258795v1.{" "}
+          </li>
+          <li>
+            News Center. (n.d.). Smartwatch can detect early signs of illness.
+            News Center.
+            https://med.stanford.edu/news/all-news/2020/12/smartwatch-can-detect-early-signs-of-illness.html.
+          </li>
+          <li>
+            StanfordBioinformatics. (n.d.).
+            StanfordBioinformatics/wearable-infection: Real-time detection of
+            infection diseases using wearables. GitHub.
+            https://github.com/StanfordBioinformatics/wearable-infection.{" "}
+          </li>
+        </div>
       </div>
     </div>
   );
