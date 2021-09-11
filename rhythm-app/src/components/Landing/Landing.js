4dc47fb099d23dd8199d804042213b12d8a43b3e
crystal <noelani.villanueva@gmail.com>
2021-09-11 15:43:25 -0700

    landing edit

--- rhythm-app/src/components/Landing/Landing.js
+++ rhythm-app/src/components/Landing/Landing.js
@@ -4,6 +4,7 @@ import AboutSummary from "../AboutSummary/AboutSummary";
 import ResearchLanding from "../ResearchLanding/ResearchLanding";
 import "./landing.css";
 function Landing() {
+  //this is the landing
   return (
     <div className="landing-main">
       <Home />
